38663520767b8764e15cf5e1a9ee56828c52757e
bramleksono <bramleksono@yahoo.co.id>
2015-05-04 04:20:11 +0000

    use textarea for comment form. fix width in new project form.

--- routes/session.php
+++ routes/session.php
@@ -12,6 +12,14 @@ $app->post('/session_starter', function () use($app) {
     }
 });
 
+/*
+$app->get('/force_session_starter', function () use($app) {
+    $_SESSION["idnumber"] = "1231230509890001";
+    $_SESSION["name"] = "Bramanto Leksono";
+    echo "You now login as ". $_SESSION["name"];
+});
+*/
+
 $app->get('/session_destroyer', function () use($app) {
     // Unset all of the session variables.
     $_SESSION = array();
